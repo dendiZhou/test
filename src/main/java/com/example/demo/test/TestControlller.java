2ed1b7181b16765566fed46ea141e81ae10bae07
dendiZhou <770896354@qq.com>
2018-07-13 14:19:02 +0800

    1
    1

--- src/main/java/com/example/demo/test/TestControlller.java
+++ src/main/java/com/example/demo/test/TestControlller.java
@@ -9,7 +9,7 @@ public class TestControlller {
 	@RequestMapping("/test")
 	public String getHello() {
                 //sys 注释123
-		return "hellomaster123456789ab";
+		return "hellomaster123456789abc";
 
 	}
 }
